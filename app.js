2c4a80b1fcb23a4a819ca43e46a3909b6dc2f835
Yasser Al-Barbary <yasser.m.albarbary@gmail.com>
2020-01-19 13:37:45 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+var items=[];
+app.get("/", function(req,res){
+
+ var today=new Date();
+ var currentDay= today.getDay();
+ var day="";
+
+ var options = {
+   weekday: "long",
+   day:"numeric",
+   month:"long"
+ };
+ var aDay= today.toLocaleDateString("en-US", options);
+
+
+ res.render("list",{kindOfDay:aDay , newListItems:items});
+});
+
+app.post("/",function(req,res){
+  item= req.body.newItem;
+  console.log(req.body.newItem);
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function(){
+  console.log("running on port 3000");
+});
