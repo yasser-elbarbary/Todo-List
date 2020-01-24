34c9d0b2efb3190204f6eb9b523212f44ac94f5b
Yasser Al-Barbary <yasser.m.albarbary@gmail.com>
2020-01-24 22:01:42 +0000

    version 1

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
 var items=[];
+
 app.get("/", function(req,res){
 
  var today=new Date();
@@ -30,6 +36,7 @@ app.post("/",function(req,res){
   items.push(item);
   res.redirect("/");
 });
+
 
 app.listen(3000, function(){
   console.log("running on port 3000");
