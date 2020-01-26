440fdf93f971bca564ae386b9b49abc2542556ae
Yasser Al-Barbary <yasser.m.albarbary@gmail.com>
2020-01-26 03:34:31 +0000

    Add DB functionlity

--- app.js
+++ app.js
@@ -10,15 +10,25 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true});
 var items=[];
 
+//(1) create a db
+//creating a mongoDB schema
+mongoose.connect("mongodb://localhost:27017/todolistDB",{ useUnifiedTopology: true ,useNewUrlParser:true});
+const itemsSchema = {
+  name :String
+};
+//creatig a collection
+const Item = mongoose.model("Item", itemsSchema);
+
+
+//(3) in get, send items
+//(4) in post, get req.newItem and add it to db then return to (2)
+
+
 app.get("/", function(req,res){
-
+ // creatig date format
  var today=new Date();
- var currentDay= today.getDay();
- var day="";
-
  var options = {
    weekday: "long",
    day:"numeric",
@@ -26,14 +36,25 @@ app.get("/", function(req,res){
  };
  var aDay= today.toLocaleDateString("en-US", options);
 
-
- res.render("list",{kindOfDay:aDay , newListItems:items});
+ //(2) load items=[] with db.items.name
+ items=[];
+ Item.find({},function(err, foundItems){
+   foundItems.forEach(function(element){
+     console.log(element.name);
+     items.push(element.name);
+   });
+   console.log(foundItems);
+ });
+ // load the list.ejs with the day and the list of items
+ res.render("list", {kindOfDay:aDay , newListItems:items});
 });
+
 
 app.post("/",function(req,res){
   item= req.body.newItem;
   console.log(req.body.newItem);
-  items.push(item);
+  var arr = [{ name:item }];
+  Item.insertMany(arr, function(error, docs) {});
   res.redirect("/");
 });
 
