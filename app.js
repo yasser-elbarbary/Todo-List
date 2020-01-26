2de2a70ac9e63ad57365bb0da1b69c646c1255bc
Yasser Al-Barbary <yasser.m.albarbary@gmail.com>
2020-01-26 05:42:03 +0000

    Add delete feature

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-var items=[];
 
 //(1) create a db
 //creating a mongoDB schema
@@ -37,27 +36,32 @@ app.get("/", function(req,res){
  var aDay= today.toLocaleDateString("en-US", options);
 
  //(2) load items=[] with db.items.name
- items=[];
+ console.log("1");
  Item.find({},function(err, foundItems){
    foundItems.forEach(function(element){
-     console.log(element.name);
-     items.push(element.name);
    });
-   console.log(foundItems);
+  // console.log(foundItems);
+  res.render("list", {kindOfDay:aDay , newListItems:foundItems});
+  console.log("2");
  });
  // load the list.ejs with the day and the list of items
- res.render("list", {kindOfDay:aDay , newListItems:items});
+ console.log("3");
 });
 
 
 app.post("/",function(req,res){
   item= req.body.newItem;
-  console.log(req.body.newItem);
+  //console.log(req.body.newItem);
   var arr = [{ name:item }];
   Item.insertMany(arr, function(error, docs) {});
   res.redirect("/");
 });
 
+app.post("/delete" , function(req,res){
+  let toDltId = req.body.checkbox;
+  Item.findByIdAndRemove(toDltId , function(err){});
+  res.redirect("/");
+});
 
 app.listen(3000, function(){
   console.log("running on port 3000");
